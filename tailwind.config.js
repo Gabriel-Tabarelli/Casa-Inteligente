8796261a12d7e468f577675d03c67d90c41fbec7
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-08-25 12:06:43 -0300

    implementa design mais ergonomico

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,15 @@ module.exports = {
       colors:{
         'corPrimaria' : '#1D425C',
         'corFundoCard' : '#D9D9D9',
+        
       },
       height:{
         '1/10' : '10%',
         '9/10' : '90%',
-      }
+      },
+      gridTemplateRows: {
+        'custom': '30% 70%', // Define your custom grid template
+      },
     },
   },
   plugins: [],
