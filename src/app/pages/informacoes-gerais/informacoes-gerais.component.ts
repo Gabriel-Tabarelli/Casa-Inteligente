0392614d63b7a5a1aaa302dd2ce4490bfc85a18e
henriquetre <hj30032006@gmail.com>
2023-09-05 16:50:07 -0300

    A só falta a api conectar

--- src/app/pages/informacoes-gerais/informacoes-gerais.component.ts
+++ src/app/pages/informacoes-gerais/informacoes-gerais.component.ts
@@ -15,108 +15,36 @@ export class InformacoesGeraisComponent implements OnInit {
 
   }
   casa!: Casa;
-  lampadaAuxQuartos!: boolean;
-  lampadaAuxCozinha!: boolean;
-  lampadaAuxSala!: boolean;
-  comodosFor!: any;
-  dispositivoUm!: String;
-  estadoLampada!:String;
 
   ngOnInit(): void {
     this.casa = this.casaService.getCasa();
-    this.comodosFor = this.casa.comodos;
-    this.verificaLampadas();
-    this.estadoLampada="OFF";
+ 
 
   }
   faPowerOff = faPowerOff;
 
-  verificaLampadas() {
-
-    this.dispositivoUm = JSON.stringify(this.casa.comodos[0].tipo);
-
-    for (let i = 0; i <= 2; i++) {
-      this.lampadaAuxQuartos = this.casa.comodos[0].dispositivos[i].estado;
-      console.log(this.lampadaAuxQuartos);
-
-      if (this.lampadaAuxQuartos) {
-        this.setLampadaAuxQuartos(true);
-        break;
-      }
-      this.lampadaAuxCozinha = this.casa.comodos[1].dispositivos[i].estado;
-
-      if (this.lampadaAuxCozinha) {
-        this.setLampadaAuxCozinha(true);
-      }
-      this.lampadaAuxSala = this.casa.comodos[2].dispositivos[i].estado;
-
-      if (this.lampadaAuxSala) {
-        this.setLampadaAuxSala(true);
-      }
-    }
-
-
-  }
-  setLampadaAuxQuartos(estado: boolean) {
-    this.lampadaAuxQuartos = estado;
-    console.log(this.lampadaAuxQuartos);
-  }
-
-  setLampadaAuxCozinha(estado: boolean) {
-    this.lampadaAuxCozinha = estado;
-  }
-
-  setLampadaAuxSala(estado: boolean) {
-    this.lampadaAuxSala = estado;
-  }
-
   desligaTodasLampadas() {
-    for (let i = 0; i < this.casa.comodos.length; i++) {
-      for (let j = 0; j < this.casa.comodos[i].dispositivos.length; j++) {
-        this.casa.comodos[i].dispositivos[j].estado = false;
-      }
-    }
+    this.casa.comodos.forEach(comodo => {
+      comodo.dispositivos.forEach(dispositivo => {
+        dispositivo.estado = false;
+      });
+    });
     this.updateCasa();
   }
 
   validaLampadas(): boolean {
-    for (let i = 0; i < this.casa.comodos.length; i++) {
-      for (let j = 0; j < this.casa.comodos[i].dispositivos.length; j++) {
-        if (this.casa.comodos[i].dispositivos[j].estado) {
-          return true;
-        }
-      }
-    }
-    return false;
+    return this.casa.comodos.some(comodo => this.validaDispositivos(comodo));
   }
 
   validaDispositivos(comodo: Comodo):boolean{
     return comodo.dispositivos.some((dispositivo) => dispositivo.estado);
   }
 
-  mudaEstadoLampada(comodo:String) {
-    console.log(comodo);
-    if(comodo=="Quarto"){
-      this.casa.comodos[0].dispositivos.forEach(dispositivo => {
-        dispositivo.estado = !dispositivo;
+  mudaEstadoLampada(comodo:Comodo) {
+   comodo.dispositivos.forEach(dispositivo => {
+        dispositivo.estado = !dispositivo.estado;
       });
-
-  }else if(comodo=="Cozinha"){
-
-    if(this.forQueMudaEstadoLampada(1)){
-      this.estadoLampada="ON";
-    }else{
-      this.estadoLampada="OFF";
-    }
-
-  }else if(comodo=="Sala"){
-    if(this.forQueMudaEstadoLampada(2)){
-      this.estadoLampada="ON";
-    }else{
-      this.estadoLampada="OFF";
-    }
-  }
-    this.verificaLampadas();
+    
     this.updateCasa();
 
   }
@@ -124,20 +52,4 @@ export class InformacoesGeraisComponent implements OnInit {
   updateCasa(): void {
     this.casaService.updateCasa(this.casa);
   }
-
-  forQueMudaEstadoLampada(x: any): any{
-    for (let i = 0; i < this.casa.comodos[x].dispositivos.length; i++) {
-      if (!this.casa.comodos[x].dispositivos[i].estado) {
-        return this.casa.comodos[x].dispositivos[i].estado = true;
-        
-        
-      } else {
-       return this.casa.comodos[x].dispositivos[i].estado = false;
-      }
-    }
-  }
-
-
-
-
 }
